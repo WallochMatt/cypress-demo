bfdf5a66a4a86312d9f412d2ed0d001c7f3640b8
WallochMatt <matthewrwalloch@gmail.com>
2022-10-28 11:17:25 -0500

    feat: Completed base tasks

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
 describe('goes to localhost', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('p')
+    cy.get('.App-link')
+    .contains('Learn React')
+    cy.get('button')
+    .click('center')
   })
 })
